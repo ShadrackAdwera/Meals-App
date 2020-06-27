c89e1341e8a286553b775fb25f48dd0ab89ffb87
FACTS  LIMITED <admin@factsafrica.com>
2020-06-27 18:37:41 +0300

    pass data to meal detail screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-
+import MealItem from '../components/MealItem';
 import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = (props) => {
   const renderMealItem = (itemData) => {
     return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
+      <MealItem
+        title={itemData.item.title}
+        image ={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({routeName: 'MealDetail', params: {
+            mealId: itemData.item.id
+          }})
+        }}
+      />
     );
   };
 
@@ -21,6 +30,7 @@ const CategoryMealsScreen = (props) => {
   return (
     <View style={styles.screen}>
       <FlatList
+        style={{ width: '100%' }}
         data={displayMeals}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+
 
 const MealDetail = (props) => {
+    const mealId = props.navigation.getParam('mealId')
+
+    const foundMeal = MEALS.find(meal=>{
+        return meal.id === mealId
+    })
+
  return (
      <View style={styles.screen}>
-         <Text>Meal Detail Screen</Text>
-         <Button title='Go to Home' onPress={()=> {
-             props.navigation.popToTop()
-         }}/>
+         <Text>{foundMeal.title}</Text>
      </View>
  )
-
-
 };
+
+MealDetail.navigationOptions = (navigationData) => {
+    const mealId = navigationData.navigation.getParam('mealId')
+    const foundMeal = MEALS.find(meal=>{
+        return meal.id === mealId
+    })
+    return {
+        headerTitle: foundMeal.title
+    } 
+}
 
 const styles = StyleSheet.create({
     screen: {
